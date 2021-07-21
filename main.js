c2a42bde3108c7e032e764ab11d5463ce2c20528
Nathan Blackman <nathanpgblackman@gmail.com>
2021-07-20 20:48:11 -0500

    Wrote all objects of teammates

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 console.log("Console log!");
+
+const teamTwo = [{
+    id: 1,
+    name: "Nathan",
+    favoriteColor: "red",
+    email: "divorcedlawyer1988@hotmail.com",
+    uniqueThing: "I produce and edit videos"
+},
+{
+    id: 2 ,
+    name: "Joe",
+    email: "josephgibbs188@gmail.com",
+    favoriteColor: "blue",
+    uniqueThing: "Burned arm"
+},
+{
+    id: 3,
+    name: "Elle",
+    email: "hello@ellelawrence.com",
+    favoriteColor: "black",
+    uniqueThing: "full sleeve of tattoos"
+},
+{
+    id: 4,
+    name: "madden",
+    email: "maddenpurcell@gmail.com",
+    favoriteColor: "Hunter Green",
+    uniqueThing: "Has pet Gecko named 'Gre'"
+}];
