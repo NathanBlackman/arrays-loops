90f84d79d14b0e7d703ac4743ac7526cc0d36d13
Nathan Blackman <nathanpgblackman@gmail.com>
2021-07-20 20:55:29 -0500

    Added images

--- main.js
+++ main.js
@@ -5,26 +5,30 @@ const teamTwo = [{
     name: "Nathan",
     favoriteColor: "red",
     email: "divorcedlawyer1988@hotmail.com",
-    uniqueThing: "I produce and edit videos"
+    uniqueThing: "I produce and edit videos",
+    image: "https://avatars.githubusercontent.com/u/86999347?v=4"
 },
 {
     id: 2 ,
     name: "Joe",
     email: "josephgibbs188@gmail.com",
     favoriteColor: "blue",
-    uniqueThing: "Burned arm"
+    uniqueThing: "Burned arm",
+    image: "https://avatars.githubusercontent.com/u/83508920?v=4"
 },
 {
     id: 3,
     name: "Elle",
     email: "hello@ellelawrence.com",
     favoriteColor: "black",
-    uniqueThing: "full sleeve of tattoos"
+    uniqueThing: "full sleeve of tattoos",
+    image: "https://avatars.githubusercontent.com/u/86808230?v=4"
 },
 {
     id: 4,
     name: "madden",
     email: "maddenpurcell@gmail.com",
     favoriteColor: "Hunter Green",
-    uniqueThing: "Has pet Gecko named 'Gre'"
+    uniqueThing: "Has pet Gecko named 'Gre'",
+    image: "https://avatars.githubusercontent.com/u/86082231?v=4"
 }];
